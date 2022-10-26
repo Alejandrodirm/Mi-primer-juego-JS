d66ce61db45adce7d33e74c8ec8b48590d23e040
Alejandro Domin <alejandrodirm52@gmail.com>
2022-10-26 19:30:46 +0200

    1 part

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,16 @@
 //https://codepen.io/Draimy/pen/BQBBqL
+var score = 0;
+function clickBtn() {
+  score++;
+  document.getElementById("score").innerHTML="Poder Cultivado: " + score;
+  if(score>100){
+    document.body.style.backgroundColor = "#33E0FF";
+  }
+  if(score>1000){
+    document.body.style.backgroundColor = "#AF33FF";
+  }
+  if(score>10000){
+    document.body.style.backgroundColor = "#FFB833";
+  }
+}
+
